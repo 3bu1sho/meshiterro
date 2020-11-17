e5edaa3448546221802f4ee3202a264ff7171b32
3bu1sho <3bu1sho@gmail.com>
2020-11-17 17:29:08 +0000

    [Finish]4-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd2b34e68ce46b4dac4db2b593825d7c26e4a431a10ab7d256fbfbec8f636685ba5bd65ea771db13a94594f1f13ab2340523f7f04fbb23e5e9a80355e6cdcaf20'
